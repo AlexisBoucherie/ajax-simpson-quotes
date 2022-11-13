fbdaebe28db8ed396c57067f8c6e9ed8e6212a02
AlexisBoucherie <alexis.boucherie@gmail.com>
2022-11-13 11:30:27 +0100

    get data from api and set reload button

---
+++ axios-api.js
@@ -0,0 +1,22 @@
+// This function loads pokemon data from the Pokemon API
+function fetchPokemonJSON() {
+    // Feel free to download this HTML and edit it, to use another Pokemon ID
+    const url = 'https://simpsons-quotes-api.herokuapp.com/quotes';
+    axios.get(url)
+        .then(function(response) {
+            return response.data[0]; // response.data instead of response.json() with fetch
+        })
+        .then(function(simpson) {
+            console.log('data decoded from JSON:', simpson);
+
+            // Build a block of HTML
+            const simpsonHtml = `
+        <p><strong>${simpson.character}</strong></p>
+        <img src="${simpson.image}" />
+        <p>${simpson.quote}</p>
+      `;
+            document.querySelector('#simpson').innerHTML = simpsonHtml;
+        });
+}
+
+fetchPokemonJSON();
